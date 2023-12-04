5bae7971274d40537fdb4bc270dd3d5a6d42fd2b
Cashion <cashion1406@gmail.com>
2023-12-04 22:53:56 +0700

    specical character validation+ update workflow dto

--- src/modules/user/controller/dtos/user.dtos.ts
+++ src/modules/user/controller/dtos/user.dtos.ts
@@ -1,18 +1,13 @@
-/* eslint-disable prettier/prettier */
-
 import { ApiProperty } from '@nestjs/swagger/dist/decorators/api-property.decorator'
 
 export class UserDTO {
-
 	constructor(props: Partial<UserDTO>) {
 		Object.assign(this, props)
 	}
 
-    @ApiProperty()
+	@ApiProperty()
 	id: number
 
 	@ApiProperty()
 	name: string
-
 }
-
--- src/modules/workflow/controller/dtos/create-workflow-step.dtos.ts
+++ src/modules/workflow/controller/dtos/create-workflow-step.dtos.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { SuccessResponseDTO } from "@libs"
 import { ApiProperty, PartialType } from "@nestjs/swagger"
-import { Allow, ArrayNotEmpty, IsArray, IsNotEmpty, IsOptional, Length, ValidateNested } from "class-validator"
+import { Allow, ArrayNotEmpty, IsArray, IsNotEmpty, IsOptional, Length, Matches, ValidateNested } from "class-validator"
 import { CreateStepRequestDTO } from "src/modules/step/controller/dtos/create-step.dtos"
 import { WorkflowDto } from "./workflow.dtos"
 import { Type } from "class-transformer"
@@ -9,6 +9,9 @@ import { Type } from "class-transformer"
 export class CreateWorkflowStepRequestDTO{
     @ApiProperty()
 	@Length(5,20,{message:'Workflow name must be between 0 and 20 characters'})
+	@Matches(/^[a-zA-Z0-9\s]+$/, {
+		message: `Workflow name can't contain specical characters`,
+	})
 	@IsNotEmpty({message:'Workflow name cannot be empty. Please fill in the context and try again!'})
 	name: string
 
